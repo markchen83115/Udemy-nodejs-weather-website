fb28b505c7f9cb5fdf22266d657d59b5a0571f48
markchen83115 <markchen83115@gmail.com>
2020-02-08 17:27:20 +0800

    Add hightest/lowest temperature in forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) { 
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. The highest temperature is ' + body.daily.data[0].temperatureHigh + ' and the lowest emperature is ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
